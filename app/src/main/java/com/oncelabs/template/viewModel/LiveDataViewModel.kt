5929e9051de54611bea7bbbae020c6d8564c6aa8
JoeBakalor <jmbakalor@gmail.com>
2022-09-30 10:24:15 -0500

    cleanup LiveDataViewModel

--- app/src/main/java/com/oncelabs/template/viewModel/LiveDataViewModel.kt
+++ app/src/main/java/com/oncelabs/template/viewModel/LiveDataViewModel.kt
@@ -13,10 +13,6 @@ import kotlinx.coroutines.launch
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 
-data class BeaconChartData(
-    var name: String,
-    var data: ADXL367Data
-)
 
 @HiltViewModel
 class LiveDataViewModel @Inject constructor(
@@ -50,46 +46,6 @@ class LiveDataViewModel @Inject constructor(
         }
     }
 
-//    private fun observeDataFor(adxL367: ADXL367){
-//        viewModelScope.launch {
-//            adxL367.adxlData.collect {
-//                val deviceMap = _activeBeacons.value
-//                it?.let { newBeaconData ->
-//                    deviceMap?.let { ab ->
-//                        ab[adxL367.address]?.let {
-//                            if (it.currentIndex < 50){
-//                                it.currentIndex += 1
-//                                val test = it.x
-//                                it.rssi.add(Pair(it.currentIndex.toFloat(), newBeaconData.rssi.toFloat()))
-//                                it.temp.add(Pair(it.currentIndex.toFloat(), newBeaconData.temp))
-//                                it.x.add(Pair(it.currentIndex.toFloat(), newBeaconData.xAccel))
-//                                it.y.add(Pair(it.currentIndex.toFloat(), newBeaconData.yAccel))
-//                                it.z.add(Pair(it.currentIndex.toFloat(), newBeaconData.zAccel))
-//
-//                            } else {
-//                                it.currentIndex = 0
-//                                clearData(it)
-//                                it.rssi.add(Pair(it.currentIndex.toFloat(), newBeaconData.rssi.toFloat()))
-//                                it.temp.add(Pair(it.currentIndex.toFloat(), newBeaconData.temp))
-//                                it.x.add(Pair(it.currentIndex.toFloat(), newBeaconData.xAccel))
-//                                it.y.add(Pair(it.currentIndex.toFloat(), newBeaconData.yAccel))
-//                                it.z.add(Pair(it.currentIndex.toFloat(), newBeaconData.zAccel))
-//                            }
-//                        }
-//                    }
-//                }
-//                if (deviceMap != null) {
-//                    val list: MutableList<BeaconChartData> = mutableListOf()
-//                    deviceMap.map {
-//                        list.add(it.value)
-//                    }
-//                    _beacons.value = list
-//                }
-//                _activeBeacons.postValue(deviceMap)
-//            }
-//        }
-//    }
-
     private fun observeDataFor(adxL367: ADXL367){
         viewModelScope.launch {
             adxL367.historicalAdxlData.collect {
@@ -100,38 +56,3 @@ class LiveDataViewModel @Inject constructor(
         }
     }
 }
-
-
-//NanoBeaconManager.adxlData.collect{
-//
-//    val t = _temp.value?.toMutableList()
-//    t?.add(Pair(index.toFloat(), it.temp))
-//    _temp.postValue(t?.toList())
-//
-//    val r = _rssi.value?.toMutableList()
-//    r?.add(Pair(index.toFloat(), it.rssi.toFloat()))
-//    _rssi.postValue(r?.toList())
-//
-//    val x = _x.value?.toMutableList()
-//    x?.add(Pair(index.toFloat(), it.xAccel.toFloat()))
-//    _x.postValue(x?.toList())
-//
-//    val y = _y.value?.toMutableList()
-//    y?.add(Pair(index.toFloat(), it.yAccel.toFloat()))
-//    _y.postValue(y?.toList())
-//
-//    val z = _z.value?.toMutableList()
-//    z?.add(Pair(index.toFloat(), it.zAccel.toFloat()))
-//    _z.postValue(z?.toList())
-//
-//    if (index < 50) {
-//        index++
-//    } else {
-//        index = 0
-//        _temp.value = listOf()
-//        _rssi.value = listOf()
-//        _x.value = listOf()
-//        _y.value = listOf()
-//        _z.value = listOf()
-//    }
-//}
