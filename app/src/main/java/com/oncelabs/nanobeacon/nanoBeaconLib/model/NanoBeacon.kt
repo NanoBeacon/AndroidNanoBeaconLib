1169dad684b5fe3f2ed96a56a4938bf035063ee1
JoeBakalor <jmbakalor@gmail.com>
2022-10-06 07:30:40 -0500

    add est adv interval to NanoBeacon base class

--- app/src/main/java/com/oncelabs/nanobeacon/nanoBeaconLib/model/NanoBeacon.kt
+++ app/src/main/java/com/oncelabs/nanobeacon/nanoBeaconLib/model/NanoBeacon.kt
@@ -16,16 +16,51 @@ open class NanoBeacon(
 
     private val TAG = NanoBeacon::class.simpleName
 
-    /* Expose flow for observing real-time RSSI updates*/
-    private val _rssiFlow = MutableStateFlow<Int?>(null)
-    override val rssiFlow = _rssiFlow.asStateFlow()
-
     /* Expose flow for observing real-time advertisement data */
     private val _beaconDataFlow = MutableStateFlow<NanoBeaconData?>(null)
     override val beaconDataFlow = _beaconDataFlow.asStateFlow()
 
+    /* Expose flow for observing real-time RSSI updates*/
+    private val _rssiFlow = MutableStateFlow<Int?>(null)
+    override val rssiFlow = _rssiFlow.asStateFlow()
+
+    /* Expose flow for observing estimated advertisement interval */
+    private val _estimatedAdvIntervalFlow = MutableStateFlow<Int?>(null)
+    override val estimatedAdvIntervalFlow = _estimatedAdvIntervalFlow.asStateFlow()
+
+    private val TIMESTAMP_COUNT = 10
+    private var advTimestamps: MutableList<Long> = mutableListOf()
+
     override fun newBeaconData(beaconData: NanoBeaconData) {
         _beaconDataFlow.value = beaconData
         _rssiFlow.value = beaconData.rssi
+        updateAdvInterval(beaconData.timeStamp)
+    }
+
+    private fun updateAdvInterval(timestamp: Long){
+
+        if (advTimestamps.count() < TIMESTAMP_COUNT){
+            // Add new timestamp
+            advTimestamps.add(timestamp)
+        } else {
+            // Remove oldest timestamp and add new
+            advTimestamps.removeFirst()
+            advTimestamps.add(timestamp)
+        }
+
+        // Compute average delta between timestamps
+        if (advTimestamps.count() > 1) {
+            val deltaTimestamps = mutableListOf<Long>()
+
+            advTimestamps.indices.forEach { index ->
+                if (index < advTimestamps.count() - 1 ){
+                    deltaTimestamps.add((advTimestamps[index + 1] - advTimestamps[index]))
+                }
+            }
+            val advIntervalAvg = (deltaTimestamps.sum())/deltaTimestamps.count()
+
+            // Update adv interval estimate flow
+            _estimatedAdvIntervalFlow.value = advIntervalAvg.toInt()
+        }
     }
 }
