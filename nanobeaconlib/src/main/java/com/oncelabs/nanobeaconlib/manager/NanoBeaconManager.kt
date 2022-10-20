0ddcc7c769cee2a55cf1afd425bf87ef8a3607de
Seth Polyniak <emailspolyniak@gmail.com>
2022-10-20 15:25:36 -0400

    [ITNA-61] - add message to turn on BT when off on startup

--- nanobeaconlib/src/main/java/com/oncelabs/nanobeaconlib/manager/NanoBeaconManager.kt
+++ nanobeaconlib/src/main/java/com/oncelabs/nanobeaconlib/manager/NanoBeaconManager.kt
@@ -63,6 +63,20 @@ object NanoBeaconManager: NanoBeaconManagerInterface, NanoBeaconDelegate {
         if (!bluetoothAdapter.isEnabled){
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             NanoBeaconManager.getContext.let {
+                if (ActivityCompat.checkSelfPermission(
+                        getContext(),
+                        Manifest.permission.BLUETOOTH_CONNECT
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
                 (it() as Activity).startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
             }
         }
